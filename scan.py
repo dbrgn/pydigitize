b8adde27abac0b66a70bdf27d48a687faa66af72
Danilo Bargen <mail@dbrgn.ch>
2014-11-01 16:57:26 +0100

    Converted function to class

--- scan.py
+++ scan.py
@@ -3,7 +3,7 @@
 """pydigitize.
 
 Usage:
-    scan.py [options]
+    scan.py -o OUTPUT [options]
 
 Options:
     -h --help      Show this help.
@@ -19,8 +19,6 @@ Options:
     --debug        Debug output
 
 """
-from __future__ import print_function, division, absolute_import, unicode_literals
-
 import sys
 import glob
 import datetime
@@ -29,8 +27,10 @@ import logging
 
 import docopt
 from sh import cd, mkdir, scanimage, tiffcp, tiff2pdf, mv
-import sh; ocrmypdf = getattr(sh, 'OCRmyPDF.sh')
+import sh
 
+
+ocrmypdf = getattr(sh, 'OCRmyPDF.sh')
 
 logger = logging.getLogger('pydigitize')
 
@@ -40,65 +40,123 @@ OUTPUT_BASE = '/home/danilo/brscan'
 TIMESTAMP = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
 
 
-def scan(resolution, device, output):
+class Scan:
 
-    # Validate args
-    def _invalid_res():
-        print('Invalid resolution. Please use one of {!r}.'.format(VALID_RESOLUTIONS))
-        sys.exit(1)
-    try:
-        if int(resolution) not in VALID_RESOLUTIONS:
+    def __init__(self, resolution, device, output):
+        """
+        Initialize scan class.
+
+        Added attributes:
+
+        - resolution
+        - device
+        - output_path
+
+        """
+        # Validate and store resolution
+        def _invalid_res():
+            print('Invalid resolution. Please use one of {!r}.'.format(VALID_RESOLUTIONS))
+            sys.exit(1)
+        try:
+            if int(resolution) not in VALID_RESOLUTIONS:
+                _invalid_res()
+        except ValueError:
             _invalid_res()
-    except ValueError:
-        _invalid_res()
-    if os.path.isdir(output):
-        output_path = os.path.join(output, TIMESTAMP + '.pdf')
-    elif os.path.isdir(os.path.dirname(output)):
-        output_path = output
-    else:
-        print('Output directory must already exist.')
-        sys.exit(1)
+        else:
+            self.resolution = resolution
 
-    # Prepare directories
-    print('Creating directory...')
-    workdir = os.path.join(OUTPUT_BASE, TIMESTAMP)
-    mkdir(workdir, parents=True)
-    cd(workdir)
+        # Store device
+        self.device = device
 
-    # Scan pages
-    print('Scanning...')
-    scanimage_args = {
-        'x': 210, 'y': 297,
-        'device_name': device,
-        'batch': True,
-        'format': 'tiff',
-        'resolution': resolution,
-        '_ok_code': [0, 7],
-    }
-    logger.debug('Scanimage args: %r' % scanimage_args)
-    scanimage(**scanimage_args)
+        # Validate and store output path
+        if os.path.isdir(output):
+            self.output_path = os.path.join(output, TIMESTAMP + '.pdf')
+        elif os.path.isdir(os.path.dirname(output)):
+            self.output_path = output
+        else:
+            print('Output directory must already exist.')
+            sys.exit(1)
+        logger.debug('Output path: %s', self.output_path)
 
-    # Combine tiffs into single multi-page tiff
-    print('Combining image files...')
-    files = sorted(glob.glob('out*.tif'))
-    logger.debug('Joining %r', files)
-    tiffcp(files, 'output.tif', c='lzw')
+    def prepare_directories(self):
+        """
+        Prepare the temporary output directories.
 
-    # Convert tiff to pdf
-    print('Converting to PDF...')
-    tiff2pdf('output.tif', p='A4', o='output.pdf')
-    # TODO: use convert instead?
+        Added attributes:
 
-    # Do OCR
-    print('Running OCR...')
-    ocrmypdf('-l', 'deu', '-o', resolution, '-d', '-c', 'output.pdf', 'clean.pdf')
+        - workdir
 
-    # Move file
-    print('Moving resulting file...')
-    cd('..')
-    mv('%s/clean.pdf' % workdir, output_path)
+        """
+        print('Creating temporary directory...')
+        self.workdir = os.path.join(OUTPUT_BASE, TIMESTAMP)
+        mkdir(self.workdir, parents=True)
 
-    print('Done: %s' % output_path)
+    def scan_pages(self):
+        """
+        Scan pages using ``scanimage``.
+        """
+        print('Scanning...')
+        scanimage_args = {
+            'x': 210, 'y': 297,
+            'device_name': self.device,
+            'batch': True,
+            'format': 'tiff',
+            'resolution': self.resolution,
+            '_ok_code': [0, 7],
+        }
+        logger.debug('Scanimage args: %r' % scanimage_args)
+        scanimage(**scanimage_args)
+
+    def combine_tiffs(self):
+        """
+        Combine tiffs into single multi-page tiff.
+        """
+        print('Combining image files...')
+        files = sorted(glob.glob('out*.tif'))
+        logger.debug('Joining %r', files)
+        tiffcp(files, 'output.tif', c='lzw')
+
+    def convert_tiff_to_pdf(self):
+        """
+        Convert tiff to pdf.
+
+        TODO: use convert instead?
+
+        """
+        print('Converting to PDF...')
+        tiff2pdf('output.tif', p='A4', o='output.pdf')
+
+    def do_ocr(self):
+        """
+        Do character recognition (OCR) with ``OCRmyPDF.sh``.
+        """
+        print('Running OCR...')
+        ocrmypdf('-l', 'deu', '-o', self.resolution, '-d', '-c', 'output.pdf', 'clean.pdf')
+
+    def process(self):
+        # Prepare directories
+        self.prepare_directories()
+        cd(self.workdir)
+
+        # Scan pages
+        self.scan_pages()
+
+        # Combine tiffs into single multi-page tiff
+        self.combine_tiffs()
+
+        # Convert tiff to pdf
+        self.convert_tiff_to_pdf()
+
+        # Do OCR
+        self.do_ocr()
+
+        # Move file
+        print('Moving resulting file...')
+        cd('..')
+        mv('%s/clean.pdf' % self.workdir, self.output_path)
+
+        print('Done: %s' % self.output_path)
+
 
 if __name__ == '__main__':
     args = docopt.docopt(__doc__, version='pydigitize 0.1')
@@ -110,4 +168,6 @@ if __name__ == '__main__':
         logging.basicConfig(level=logging.WARNING)
     logger.debug('Command line args: %r' % args)
     default_output = os.path.join(OUTPUT_BASE, TIMESTAMP)
-    scan(resolution=args['-r'], device=args['-d'], output=args['-o'] or default_output)
+
+    scan = Scan(resolution=args['-r'], device=args['-d'], output=args['-o'] or default_output)
+    scan.process()
